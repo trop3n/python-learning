1464225500f42095927c60bf807c4ef390f7eec8
trop3n <jasonkimm12@gmail.com>
2024-11-26 11:38:31 -0600

    updating repo with all recent study projects

---
+++ concepts/docstrings.py
@@ -0,0 +1,5 @@
+def square(n):
+    '''Take a number n and return the square of n.'''
+    return n**2
+
+print(square.__doc__)
---
+++ thehardway/ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Jason D. Kimm'
+my_age = 33 # not a lie
+my_height = 74 # inches
+my_weight = 215 # pounds
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# This like is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight}, I get {total}.")
