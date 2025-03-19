5d3537ef404f5102ddc5b2b78d380d3ebbe865cf
trop3n <jasondkimm@proton.me>
2025-03-19 15:27:49 -0500

    extend and append methods

--- python-books/python_for_netsec/chapters/ch01/lists.py
+++ python-books/python_for_netsec/chapters/ch01/lists.py
@@ -3,29 +3,22 @@ protocolList.append("FTP")
 protocolList.append("SSH")
 protocolList.append("SMTP")
 protocolList.append("HTTP")
-# print(protocolList)
-
 protocolList.sort()
-# print(protocolList)
 
-# print(type(protocolList))
-# print(len(protocolList))
 
 position = protocolList.index('SSH')
 # print("SSH Position "+str(position))
 
-protocolList.remove("SSH")
-# print(protocolList)
 
 count = len(protocolList)
 # print("Protocol elements "+ str(count))
 
-print(protocolList)
-protocolList.pop()
-print(protocolList)
-protocolList.reverse()
-print(protocolList)
-
 
 for protocol in protocolList:
     print(protocol)
+
+responses = [200, 400, 403, 500, 503]
+responses.extend([504,505])
+print(responses)
+responses.insert(6, 300)
+print(responses)
