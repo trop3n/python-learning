a34f4e0110c4bc6596b0dfa108ef3c5eab41269b
trop3n <jasondkimm@proton.me>
2025-04-09 16:21:55 -0500

    exception handling, working with files

---
+++ python-books/python_for_netsec/chapters/ch01/read_file_exception.py
@@ -0,0 +1,9 @@
+try:
+    file_handle = open("myfile.txt", "r")
+except IOError as exception:
+    print("Exception IOError: Unable to read from myfile ", exception)
+except Exception as exception:
+    print("Exception: ", exception)
+else:
+    print("File read successfully")
+    file_handle.close()
