70a32c7c87520b178546061a00ce5d1c3591e8f1
trop3n <jasondkimm@proton.me>
2025-06-10 15:04:17 -0500

    script that sends ICMP requests to three IP addresses within the 192.168.12 network range

---
+++ python-books/python_for_netsec/chapters/ch02/subprocess_ping_network.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from subprocess import Popen, PIPE
+import sys
+
+print("Operating system:", sys.platform)
+if sys.platform.startswith("linux"):
+    command_ping = '/bin/ping'
+elif sys.platform == "darwin":
+    command_ping = '/sbin/ping'
+elif os.name == "nt":
+    command_ping = 'ping'
+for ip in range(1,4):
+    ipAddress = '192.168.18.'+str(ip)
+    print("Scanning %s " %(ipAddress))
+    subprocess = Popen([command_ping, '-c 1',ipAddress], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = subprocess.communicate(input=None)
+    print(stdout)
+    if b"bytes from " in stdout:
+        print("The IP Address %s has responded with a ECHO_REPLY!" %(stdout.split()[1]))
