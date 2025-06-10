0e6b75b9bb5214f3a81d023e1bcc8a6e180b367b
trop3n <jasondkimm@proton.me>
2025-06-10 15:28:30 -0500

    script to automate venv setup

---
+++ python-books/python_for_netsec/chapters/ch02/subprocess_setup_venv.py
@@ -0,0 +1,17 @@
+import subprocess
+from pathlib import Path
+VENV_NAME = '.venv'
+REQUIREMENTS = 'requirements.txt'
+process = subprocess.run(['which', 'python3'], capture_output=True, text=True)
+if process.returncode != 0:
+    raise OSError('Sorry python3 is not installed')
+python_process = process.stdout.strip()
+print(f'Python found in: {python_process}')
+
+process = subprocess.run('echo "$SHELL"', shell=True, capture_output=True, text=True)
+shell_bin = process.stdout.split('/')[-1]
+create_venv = subprocess.run([python_process, '-m', 'venv', VENV_NAME], check=True)
+if create_venv.returncode == 0:
+    print(f'Your venv {VENV_NAME} has been created')
+else:
+    print(f'Your venv {VENV_NAME} has not been created')
