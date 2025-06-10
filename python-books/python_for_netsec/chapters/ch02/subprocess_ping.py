568590011a8da8ae3e2f15d9b3d2f9f5c9aa5a11
trop3n <jasondkimm@proton.me>
2025-06-10 14:49:50 -0500

    fixed sheel to shell

--- python-books/python_for_netsec/chapters/ch02/subprocess_ping.py
+++ python-books/python_for_netsec/chapters/ch02/subprocess_ping.py
@@ -9,7 +9,7 @@ elif os.name == "nt":
     command_ping = 'ping'
 ping_parameter = '-c 1'
 domain = "www.google.com"
-p = subprocess.Popen([command_ping, ping_parameter, domain], sheel=False, stderr=subprocess.PIPE)
+p = subprocess.Popen([command_ping, ping_parameter, domain], shell=False, stderr=subprocess.PIPE)
 out = p.stderr.read(1)
 sys.stdout.write(str(out.decode('utf-8')))
 sys.stdout.flush()
