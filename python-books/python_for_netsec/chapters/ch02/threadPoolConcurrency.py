c69cc52a16829391fa66bb87db6fa033aa594b4b
trop3n <jasondkimm@proton.me>
2025-06-16 15:38:13 -0500

    define a task() function that allows us to use the threading.get_ident() method to show current thread identifier

--- python-books/python_for_netsec/chapters/ch02/threadPoolConcurrency.py
+++ python-books/python_for_netsec/chapters/ch02/threadPoolConcurrency.py
@@ -7,3 +7,11 @@ def task(n):
     print("Thread Executed {}".format(threading.current_thread))
 def main():
     print("Starting ThreadPoolExecutor")
+    executor = ThreadPoolExecutor(max_workers=3)
+    future = executor.submit(task, (2))
+    future = executor.submit(task, (3))
+    future = executor.submit(task, (4))
+    print("All tasks complete")
+
+if __name__ == '__main__':
+    main()
