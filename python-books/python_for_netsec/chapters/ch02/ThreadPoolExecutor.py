71758060515a2ab3ca55137e6fcf01da09ee4d58
trop3n <jasondkimm@proton.me>
2025-06-16 15:50:56 -0500

    using 5 threads for executing the task in an asynchronous way

--- python-books/python_for_netsec/chapters/ch02/ThreadPoolExecutor.py
+++ python-books/python_for_netsec/chapters/ch02/ThreadPoolExecutor.py
@@ -6,3 +6,10 @@ def execute(name):
     value = randint(0, 1000)
     thread_name = threading.current_thread().name
     print(f'I am {thread_name} and my value is {value}')
+    return (thread_name, value)
+with ThreadPoolExecutor(max_workers=5) as executor:
+    futures = [executor.submit(executre,f'T{name}') for name in range(5)]
+    for future in as_completed(futures):
+        name, value = future.result()
+        print(f'Thread {name} returned {value}')
+        
