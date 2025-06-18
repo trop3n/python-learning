f7479f4ac6895ede8d4e5aa78162a339df5d979a
trop3n <jasondkimm@proton.me>
2025-06-17 19:47:31 -0500

    port scanning with sockets

--- python-books/python_for_netsec/chapters/ch03/socket_ports_open.py
+++ python-books/python_for_netsec/chapters/ch03/socket_ports_open.py
@@ -2,3 +2,7 @@ import socket
 ip = '127.0.0.1'
 portlist = [21,22,23,80]
 for port in portlist:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.conenct_ex((ip, port))
+    print(port,":", result)
+    sock.close()
